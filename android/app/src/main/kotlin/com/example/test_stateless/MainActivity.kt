c34f5cd696681c8eac9bd7943e33ccc2e0119b28
Yousuf Omer <jodeveloper8@gmail.com>
2024-02-03 15:26:07 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/test_stateless/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_stateless
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+
+  int _counter = 0;
+  final textKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    log('rebuild');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Flutter Demo Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              key: textKey,
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _counter++;
+          (context as Element).markNeedsBuild();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
